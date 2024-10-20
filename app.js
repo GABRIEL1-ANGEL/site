3729d8f04ff07963a2d3a52d3f585f1442ee2f77
Onwuka ekene <onwukadavid732@gmail.com>
2024-10-20 13:26:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+document.querySelector("button").addEventListener("click", trick);
+
+function trick () {
+    prompt("Why'd you click this button?");
+    alert("You're wicked");
+    prompt("What will you do to atone for what you've done?");
+    alert("Look at your life!");
+}
+
+
+
